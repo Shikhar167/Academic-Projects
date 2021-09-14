a404df2f6e391d1e838324fddaa52a818e116d91
Shikhar Singh <50616362+Shikhar167@users.noreply.github.com>
2021-09-14 05:40:13 +0530

    Delete Inventory Management System directory

--- Inventory Management System/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', 'root' );             // Set database password
-  define( 'DB_NAME', 'inventmgmt' );        // Set database name
-
-?>
